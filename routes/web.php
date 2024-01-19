5f2540561130b35bf5cc2b525fa9713d5eddec04
Felipe Martins <mrfelipemartins@gmail.com>
2024-01-19 19:48:33 -0300

    fix route namespace

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Zapply\Laravel\Http\Controllers\WebhookController;
 
-Route::post('webhook', 'WebhookController@handleWebhook')->name('webhook');
+Route::post('webhook', [WebhookController::class, 'handleWebhook'])->name('webhook');
